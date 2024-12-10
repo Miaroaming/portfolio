9490cb43bab1cd1d73a7958c49560b9076963acd
Miaroaming <mianzroaming@gmail.com>
2024-12-11 12:27:50 +1300

    resub for responsiveness, fixed mobile menu etc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    document.body.style.overflow = isOpen ? 'auto' : 'hidden';
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false);
+    document.body.style.overflow = 'auto';
+  }
   return (
-    <nav>
-        <Link to='/'>
-        <img src="/images/logo.png" alt="mia-lamerton-design-logo" />
-        </Link>
+    <header>
+      <nav className={`navbar ${isOpen ? "menu-open" : ""}`}>
+          <NavLink to='/' className='logo'>
+          <img src="/images/logo.png" alt="mia-lamerton-design-logo" />
+          </NavLink>
+            {/* Hamburger icon below: */}
+            <div className='menu-icon' onClick={toggleMenu}>
+                <div className={`bar bar1 ${isOpen ? 'toggle' : ''}`}></div>
+                <div className={`bar bar2 ${isOpen ? 'toggle' : ''}`}></div>
+                <div className={`bar bar3 ${isOpen ? 'toggle' : ''}`}></div>
+            </div>
+            {/* Navlinks */}
+            <ul className={`nav-pages ${isOpen ? 'active' : ''}`}>
+                {/* Must have li and everything inside for each link */}
+              <li>
+                <NavLink 
+                to='/'
+                onClick={closeMenu}
+                className={({isActive}) => (isActive ? 'active-link' : '')}
+                >Home
+                </NavLink>
+              </li>
 
-        <div className='nav-pages'>
-        <Link to='/'>Home</Link>
+      
+              <li>
+                <NavLink 
+                to='/contact'
+                onClick={closeMenu}
+                className={({isActive}) => (isActive ? 'active-link' : '')}
+                >Contact
+                </NavLink>
+              </li>
+        </ul>
 
-        {/* <Link to='/about'>About</Link> */}
-
-        {/* <Link to='/work'>My Work</Link> */}
-
-        <Link to='/contact'>Contact</Link>
-        </div>
-
-        
-    </nav>
+      </nav>
+      {isOpen && <div className='overlay' onClick={closeMenu}></div>} 
+    </header>
   )
 }
 
